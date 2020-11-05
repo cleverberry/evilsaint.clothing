d0c6fb7abaebdc4a776b6cc19d4270002288f1a2
root <root@evilsaint.clothing>
2020-11-05 09:57:58 +0000

    backup

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1605117635;s:7:"authKey";s:64:"%E.lbYt#8)#fx5n!4}fyw59ko_6@y]+@x}FrE{>*L%jKm!GA=O*fs6vAmh}o7cI0";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2309;}
